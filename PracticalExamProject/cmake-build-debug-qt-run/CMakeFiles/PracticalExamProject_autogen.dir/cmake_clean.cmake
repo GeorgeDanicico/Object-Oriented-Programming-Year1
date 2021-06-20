f6174b5e7cf128151f6f58f5fa091f7e022993bf
GeorgeDanicico <georgedanicico@yahoo.com>
2021-06-20 15:05:26 +0300

    ups

---
+++ PracticalExamProject/cmake-build-debug-qt-run/CMakeFiles/PracticalExamProject_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PracticalExamProject_autogen"
+  "PracticalExamProject_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/PracticalExamProject_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PracticalExamProject/cmake-build-debug/CMakeFiles/PracticalExamProject.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PracticalExamProject_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/PracticalExamProject_autogen.dir/ParseCache.txt"
+  "PracticalExamProject_autogen"
+  "CMakeFiles/PracticalExamProject.dir/PracticalExamProject_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/PracticalExamProject.dir/main.cpp.obj"
+  "PracticalExamProject.exe"
+  "PracticalExamProject.exe.manifest"
+  "PracticalExamProject.pdb"
+  "libPracticalExamProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PracticalExamProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
