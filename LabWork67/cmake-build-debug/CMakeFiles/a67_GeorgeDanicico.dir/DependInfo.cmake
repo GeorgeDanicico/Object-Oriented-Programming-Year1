e55a23ccc58336c7a30d5e68cba0e0869bd95c5a
GeorgeDanicico <georgedanicico@yahoo.com>
2021-06-26 16:02:55 +0300

    Update

---
+++ LabWork67/cmake-build-debug/CMakeFiles/a67_GeorgeDanicico.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/sqlite/sqlite3.c" "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/cmake-build-debug/CMakeFiles/a67_GeorgeDanicico.dir/sqlite/sqlite3.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/src/Interface/ui.cpp" "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/cmake-build-debug/CMakeFiles/a67_GeorgeDanicico.dir/src/Interface/ui.cpp.o"
+  "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/src/Service/service.cpp" "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/cmake-build-debug/CMakeFiles/a67_GeorgeDanicico.dir/src/Service/service.cpp.o"
+  "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/src/Store/store_csv.cpp" "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/cmake-build-debug/CMakeFiles/a67_GeorgeDanicico.dir/src/Store/store_csv.cpp.o"
+  "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/src/Store/store_html.cpp" "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/cmake-build-debug/CMakeFiles/a67_GeorgeDanicico.dir/src/Store/store_html.cpp.o"
+  "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/src/domain/date.cpp" "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/cmake-build-debug/CMakeFiles/a67_GeorgeDanicico.dir/src/domain/date.cpp.o"
+  "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/src/domain/domain.cpp" "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/cmake-build-debug/CMakeFiles/a67_GeorgeDanicico.dir/src/domain/domain.cpp.o"
+  "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/src/main.cpp" "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/cmake-build-debug/CMakeFiles/a67_GeorgeDanicico.dir/src/main.cpp.o"
+  "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/src/tests.cpp" "/home/george/Work/Object-Oriented-Programming-Year1/LabWork67/cmake-build-debug/CMakeFiles/a67_GeorgeDanicico.dir/src/tests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ LabWork67/cmake-build-debug/CMakeFiles/a67_GeorgeDanicico.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a67_GeorgeDanicico.dir/sqlite/sqlite3.c.o"
+  "CMakeFiles/a67_GeorgeDanicico.dir/src/Interface/ui.cpp.o"
+  "CMakeFiles/a67_GeorgeDanicico.dir/src/Service/service.cpp.o"
+  "CMakeFiles/a67_GeorgeDanicico.dir/src/Store/store_csv.cpp.o"
+  "CMakeFiles/a67_GeorgeDanicico.dir/src/Store/store_html.cpp.o"
+  "CMakeFiles/a67_GeorgeDanicico.dir/src/domain/date.cpp.o"
+  "CMakeFiles/a67_GeorgeDanicico.dir/src/domain/domain.cpp.o"
+  "CMakeFiles/a67_GeorgeDanicico.dir/src/main.cpp.o"
+  "CMakeFiles/a67_GeorgeDanicico.dir/src/tests.cpp.o"
+  "a67_GeorgeDanicico"
+  "a67_GeorgeDanicico.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/a67_GeorgeDanicico.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
